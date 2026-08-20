5af9a4956441d4ca6ad9fd8114c425476a0b6b34
Yurckevich Danila <denial1488@gmail.com>
2026-08-20 14:41:25 +0300

    Add models

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,1 +1,20 @@
 package models
+
+type Product struct {
+	ID      int    `json:"id"`
+	Model   string `json:"model"`
+	Company string `json:"company"`
+	Price   int    `json:"price"`
+}
+
+type CreateProductRequest struct {
+	Model   string `json:"model"`
+	Company string `json:"company"`
+	Price   int    `json:"price"`
+}
+
+type UpdateProductRequest struct {
+	Model   string `json:"model"`
+	Company string `json:"company"`
+	Price   int    `json:"price"`
+}
