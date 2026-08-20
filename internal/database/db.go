ca7adafe262e031a76c3c0c9375bed6128c5ce1a
Yurckevich Danila <denial1488@gmail.com>
2026-08-20 14:52:45 +0300

    Add database settings

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,1 +1,32 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type DB struct {
+	Conn *sql.DB
+}
+
+func New(dsn string, maxConnections, maxIdleConnections int, connectionTimeout string) (*DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка подключения бд: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("Ошибка Ping: %w", err)
+	}
+
+	db.SetMaxOpenConns(maxConnections)
+	db.SetMaxIdleConns(maxIdleConnections)
+
+	timeout, err := time.ParseDuration(connectionTimeout)
+	if err == nil {
+		db.SetConnMaxLifetime(timeout)
+	}
+
+	return &DB{Conn: db}, nil
+}
